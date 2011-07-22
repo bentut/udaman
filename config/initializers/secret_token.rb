4d4e59920e4b170fcf1abcbc7f54f4057e0c9c70
Benjamin Trevino <btrevino@Ben-Trevino-UHERO.local>
2011-07-21 17:34:36 -1000

    empty projectVIS@HON.A.ts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UheroDb::Application.config.secret_token = 'fee90bfe59408c3ee08d378030b5a9d68f7fd0d2b8e9e13dd90425e3f3997645f552551a70096b35d926b395b783d69e64c7723d0be7e7ea6ef24adbc6f13d42'
