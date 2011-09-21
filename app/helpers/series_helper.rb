fab92e5db2212e2b50f2103756d7b4fda61d657d
Ben Trevino <bentut@gmail.com>
2011-09-21 01:12:01 -1000

    got some variable coloring in for different levels of differences

--- app/helpers/series_helper.rb
+++ app/helpers/series_helper.rb
@@ -21,6 +21,18 @@ module SeriesHelper
     return new_words.join(" ")
   end
   
+  def aremos_color(val, aremos_val)
+    diff = (val - aremos_val).abs
+    mult = 5000
+    gray = "99"
+    red = (gray.hex + diff * mult).to_i
+    green = (gray.hex - diff * mult).to_i
+    blue = (gray.hex - diff * mult).to_i
+    red = 255 if red > 255
+    green = 20 if green < 20 
+    blue = 20 if blue < 20
+    return "##{red.to_s(16)}#{green.to_s(16)}#{blue.to_s(16)}"
+  end
   
   def navigation_by_letter
     html = "<br />"
